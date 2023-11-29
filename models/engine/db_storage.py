79f2e805294e96b31280e0ed6e2be8eacb44fc6c
Gionas361 <giovannicarmona57@gmail.com>
2023-11-29 18:49:40 -0400

    pycodestyle

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -66,7 +66,7 @@ class DBStorage:
     def reload(self):
         """Create all tables in the database
         and initialize a new session."""
-        
+
         Base.metadata.create_all(self.__engine)
         session_factory = sessionmaker(bind=self.__engine,
                                        expire_on_commit=False)
